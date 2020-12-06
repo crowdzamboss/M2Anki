bfa4d032915063b72dfed04cc50ddf2d9e772b22
crowdzamboss <75571691+crowdzamboss@users.noreply.github.com>
2020-12-06 16:59:59 +0100

    Update update_issues.py
    added base functionality. does not work yet.

--- feedback/update_issues.py
+++ feedback/update_issues.py
@@ -1,1 +1,16 @@
+from github import Github
+import os
 
+token = os.getenv('git_token')
+g = Github(token)
+repo = g.get_repo("crowdzamboss/M2Anki")
+
+"""i = repo.create_issue(
+    title="Issue Title",
+    body="Text of the body.",
+    assignee="crowdzamboss",
+    labels=[
+        repo.get_label("good first issue")
+    ]
+)
+print(i)"""
